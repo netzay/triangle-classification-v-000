c59e2c1a43407e614998e428506775dc829f1177
Ally Netzel <netzelally@gmail.com>
2018-04-20 00:18:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,15 @@ class Triangle
   attr_accessor :one, :two, :three 
   
   def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind 
+    
     
   end
-
 
 
 class TriangleError < StandarError
